a437a54fb64147d9b98a72de11baab777e88d4d1
Oener Aydogan <oener.aydogan@uni-ulm.de>
2024-01-14 18:01:34 +0100

    check if file exists in resources before executing weather task

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -3,6 +3,9 @@ package de.exxcellent.challenge;
 import de.exxcellent.challenge.utils.ArgParser;
 import org.apache.commons.cli.*;
 
+import java.io.FileNotFoundException;
+import java.net.URL;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -18,12 +21,36 @@ public final class App {
     public static void main(String... args) {
         CommandLine cmd = ArgParser.parseArgs(args);
 
-        String inputFilePath = cmd.getOptionValue("inputFile");
+        try {
+            if (cmd.hasOption("w") || cmd.hasOption("weather")) {
+                String fileName = cmd.getOptionValue("weather");
+                if (checkIfFileExists(fileName)) {
+                    System.out.println("Weather task!");
+                } else {
+                    throw new FileNotFoundException("File " + fileName + " not found. Check if file is in the " +
+                        "resources/de/exxcellent/challenge folder.");
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
 
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+    }
+
+    /***
+     * Checks if the given file exists in the resources/de/exxcellent/challenge folder
+     * @param fileName The name of the file that is checked
+     * @return true if file exists
+     */
+    private static boolean checkIfFileExists(String fileName){
+        URL fileURL = App.class.getClassLoader().getResource("de/exxcellent/challenge/" + fileName);
+        return fileURL != null;
     }
 }

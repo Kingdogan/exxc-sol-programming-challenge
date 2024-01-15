04b19c7247327b3de3044114c27db145f9fe9af6
Oener Aydogan <oener.aydogan@uni-ulm.de>
2024-01-15 23:17:59 +0100

    add weather analyzer

--- src/main/java/de/exxcellent/challenge/tasks/WeatherAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/tasks/WeatherAnalyzer.java
@@ -1,13 +1,51 @@
 package de.exxcellent.challenge.tasks;
 
+import java.util.List;
+
 public class WeatherAnalyzer {
 
   /**
-   * This method reads the given file and finds the day with the smallest temperature spread
-   * @param fileName Name of the file to read
+   * This method finds the day with the smallest temperature spread in the given data
+   * @param weatherData Data of the weather
    * @return Day with the smallest temperature spread
    */
-  public static String findDayWithSmallestTemperatureSpread(String fileName) {
-    return "";
+  public static String findDayWithSmallestTemperatureSpread(List<String[]> weatherData) {
+    // First we check if the columns Day, MxT and MnT exist
+    int columnDay = findIndex(weatherData, "Day");
+    if (columnDay == -1) {
+      throw new IllegalArgumentException("Missing column: Day");
+    }
+    int columnMxT = findIndex(weatherData, "MxT");
+    if (columnMxT == -1) {
+      throw new IllegalArgumentException("Missing column: MxT");
+    }
+    int columnMnT = findIndex(weatherData, "MnT");
+    if (columnMnT == -1) {
+      throw new IllegalArgumentException("Missing column: MnT");
+    }
+
+    float minTempSpread= 1000000000;
+    String daySmallestSpread = "";
+    for (int i = 1; i < weatherData.size(); i++) {
+      float maxTemp = Float.parseFloat(weatherData.get(i)[columnMxT]);
+      float minTemp = Float.parseFloat(weatherData.get(i)[columnMnT]);
+      float tempSpread = maxTemp - minTemp;
+      if (tempSpread < minTempSpread) {
+        daySmallestSpread = weatherData.get(i)[columnDay];
+        minTempSpread = tempSpread;
+      }
+    }
+    return daySmallestSpread;
+  }
+
+  private static int findIndex(List<String[]> data, String target) {
+    for (String[] line : data) {
+      for (int i = 0; i < line.length; i++) {
+        if (line[i].equals(target)) {
+          return i;
+        }
+      }
+    }
+    return -1;
   }
 }

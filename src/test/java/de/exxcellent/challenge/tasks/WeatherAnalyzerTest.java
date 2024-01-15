94c87509ea85321ad99e251f554eb3fcfb637dc6
Oener Aydogan <oener.aydogan@uni-ulm.de>
2024-01-15 23:17:25 +0100

    add tests missing columns for weather analyzer

--- src/test/java/de/exxcellent/challenge/tasks/WeatherAnalyzerTest.java
+++ src/test/java/de/exxcellent/challenge/tasks/WeatherAnalyzerTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WeatherAnalyzerTest {
 
@@ -22,5 +22,29 @@ public class WeatherAnalyzerTest {
     } catch (IOException | URISyntaxException e) {
       fail(e.getMessage());
     }
+  }
+
+  @Test
+  void testMissingColumnDay() {
+    List<String[]> testData = new ArrayList<>();
+    testData.add(new String[]{"MxT","MnT"});
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> WeatherAnalyzer.findDayWithSmallestTemperatureSpread(testData));
+    assertEquals("Missing column: Day", exception.getMessage());
+  }
+
+  @Test
+  void testMissingColumnMxT() {
+    List<String[]> testData = new ArrayList<>();
+    testData.add(new String[]{"Day","MnT"});
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> WeatherAnalyzer.findDayWithSmallestTemperatureSpread(testData));
+    assertEquals("Missing column: MxT", exception.getMessage());
+  }
+
+  @Test
+  void testMissingColumnMnT() {
+    List<String[]> testData = new ArrayList<>();
+    testData.add(new String[]{"Day", "MxT"});
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> WeatherAnalyzer.findDayWithSmallestTemperatureSpread(testData));
+    assertEquals("Missing column: MnT", exception.getMessage());
   }
 }

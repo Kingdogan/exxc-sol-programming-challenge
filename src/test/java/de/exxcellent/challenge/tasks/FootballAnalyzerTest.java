59d1efe8a06bdd9395c429da98a321a558b88973
Oener Aydogan <oener.aydogan@uni-ulm.de>
2024-01-15 23:44:21 +0100

    add football analyzer test

---
+++ src/test/java/de/exxcellent/challenge/tasks/FootballAnalyzerTest.java
@@ -0,0 +1,50 @@
+package de.exxcellent.challenge.tasks;
+
+import de.exxcellent.challenge.utils.io.CsvReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FootballAnalyzerTest {
+
+  @Test
+  void testFootballAnalyzer() {
+    String fileName = "football.csv";
+    try {
+      List<String[]> data = new CsvReader().readFile(fileName);
+      String team = FootballAnalyzer.findTeamWithSmallestGoalSpread(data);
+      assertEquals("Aston_Villa", team);
+    } catch (IOException | URISyntaxException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  void testMissingColumnTeam() {
+    List<String[]> testData = new ArrayList<>();
+    testData.add(new String[]{"Goals","Goals Allowed"});
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> FootballAnalyzer.findTeamWithSmallestGoalSpread(testData));
+    assertEquals("Missing column: Team", exception.getMessage());
+  }
+
+  @Test
+  void testMissingColumnGoals() {
+    List<String[]> testData = new ArrayList<>();
+    testData.add(new String[]{"Team","Goals Allowed"});
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> FootballAnalyzer.findTeamWithSmallestGoalSpread(testData));
+    assertEquals("Missing column: Goals", exception.getMessage());
+  }
+
+  @Test
+  void testMissingColumnGoalsAllowed() {
+    List<String[]> testData = new ArrayList<>();
+    testData.add(new String[]{"Team", "Goals"});
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> FootballAnalyzer.findTeamWithSmallestGoalSpread(testData));
+    assertEquals("Missing column: Goals Allowed", exception.getMessage());
+  }
+}
